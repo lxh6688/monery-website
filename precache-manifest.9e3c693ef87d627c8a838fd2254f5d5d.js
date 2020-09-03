fccd69ae9797697ac6a01eca9712ba9beec6bf76
codetiger <1272662790@qq.com>
2020-09-03 11:13:28 +0800

    deploy

---
+++ precache-manifest.9e3c693ef87d627c8a838fd2254f5d5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533f24fc89f756568d6f",
+    "url": "/morney-website/css/app.3f271ae4.css"
+  },
+  {
+    "revision": "1471ed06dd4f27bbafd5bbde19ff5fcd",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "533f24fc89f756568d6f",
+    "url": "/morney-website/js/app.ab4d0978.js"
+  },
+  {
+    "revision": "e6e76c7218746fe18b0d",
+    "url": "/morney-website/js/chunk-vendors.18c643de.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
